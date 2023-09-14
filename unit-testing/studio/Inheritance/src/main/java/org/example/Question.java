1f9f9fae539f836c7be4ecc75e5275b7ed484a75
MykaAustill <135452276+MykaAustill@users.noreply.github.com>
2023-09-13 19:55:12 -0500

    Studio practice

---
+++ unit-testing/studio/Inheritance/src/main/java/org/example/Question.java
@@ -0,0 +1,8 @@
+package org.example;
+import java.util.ArrayList;
+public class Question {
+    private String questionText;
+    private boolean isAnswered;
+    private ArrayList<String> answerSelected = new ArrayList<>();
+
+}
---
+++ unit-testing/studio/Inheritance/src/main/java/org/example/TrueFalse.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class TrueFalse extends Question{
+    private boolean isCorrect;
+
+}
